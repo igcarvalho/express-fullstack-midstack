4540f0b14c0c9f714796fc8171cda9a0bd987149
igcarvalho <igorcarvalho288@gmail.com>
2025-03-27 16:36:12 -0300

    feat: installing webpack in the project

---
+++ frontend/assets/js/bundle.js
@@ -0,0 +1,123 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/modulo1.js":
+/*!************************!*\
+  !*** ./src/modulo1.js ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__),
+/* harmony export */   idade: () => (/* binding */ idade),
+/* harmony export */   nome: () => (/* binding */ nome),
+/* harmony export */   sobrenome: () => (/* binding */ sobrenome),
+/* harmony export */   soma: () => (/* binding */ soma)
+/* harmony export */ });
+var nome = 'Igor';
+var sobrenome = 'Carvalho';
+var idade = 22;
+var cpf = '123.456.789-00';
+function soma(x, y) {
+  return x + y;
+}
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (function (x, y) {
+  return x * y;
+});
+
+
+// export class Pessoa {
+//     constructor(nome, sobrenome){
+//         this.nome = nome;
+//         this.sobrenome = sobrenome;
+//     }
+// }
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry needs to be wrapped in an IIFE because it needs to be isolated against other modules in the chunk.
+(() => {
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _modulo1__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./modulo1 */ "./src/modulo1.js");
+// import {nome,sobrenome,idade,soma,Pessoa as example} from './modulo1';
+// import * as modulo1 from './modulo1';
+// import qualquerNome from './modulo1';
+
+// const p1 = new example('Igor','Carvalho');
+// console.log(p1);
+
+// console.log(nome,sobrenome,idade);
+// console.log(soma(2,3));
+
+// console.log(qualquerNome(5,5));
+
+
+console.log((0,_modulo1__WEBPACK_IMPORTED_MODULE_0__["default"])(11, 2));
+console.log(_modulo1__WEBPACK_IMPORTED_MODULE_0__.nome, _modulo1__WEBPACK_IMPORTED_MODULE_0__.sobrenome, _modulo1__WEBPACK_IMPORTED_MODULE_0__.idade, (0,_modulo1__WEBPACK_IMPORTED_MODULE_0__.soma)(4, 4));
+})();
+
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
